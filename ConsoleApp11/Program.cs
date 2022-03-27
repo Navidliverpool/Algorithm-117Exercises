1af0382085c499cd99ab633e9c1e9a65e25e1d04
navid khodaveisi <navidliverpool@yahoo.com>
2022-03-27 02:39:28 +0100

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(First(2, 2));
+            Console.ReadLine();
+        }
+
+
+
+
+        private static int First(int num1, int num2)
+        {
+            int result;
+            if (num1 == num2)
+                return result = (num1 + num2) * 3;
+            else
+                return result = num1 + num2;
+        }
+
+
+    }
+}
